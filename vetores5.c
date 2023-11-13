b96c00bf433f07f40e4d7eddb9bc9594632859b9
AdrianoSSS <143419181+AdrianoSSS@users.noreply.github.com>
2023-11-13 11:14:45 -0300

    Add files via upload

---
+++ vetores5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+     int i =0;
+     int idades[6];
+     char nomes [6][200];
+     
+     
+
+     printf("digite os elementos do vetor : \n");
+     for(i = 0; i < 6; i++)
+     {
+        printf("Digite %d nome: \n", i+1);
+        scanf("%s", &idades[i]);
+        
+        printf("Digite %d idade : \n", i+1);
+        scanf("%d", &nomes[i]);
+      fflush(stdin);
+     }
+    
+     system ("cls || clear ");
+    
+     printf("\nEXIBINDO RESULTADOS\n");
+     for (i = 0; i < 6; i++)
+     {
+    
+        printf("nomes %d : %s\n",i +1, nomes[i]);
+        printf("idade %d : %d\n\n",i +1, idades[i]);
+     }
+     
+     return 0;
+}
